f4793c639dd8bd338f29796af77f53f2705ff6c6
Joaquim Perez <jquim1@hotmail.com>
2021-06-02 21:37:11 +0100

    update tweak

--- webapp/Component.js
+++ webapp/Component.js
@@ -11,35 +11,9 @@ sap.ui.define([
 		},
 
 		init: function() {
-			this._validateCache();
-			this._reloadConfig();
-
 			UIComponent.prototype.init.apply(this, arguments);
 			this.getRouter().initialize();
-		},
-
-		_validateCache: function() {
-			const oApplicationCache = window.applicationCache;
-			if (oApplicationCache) {
-				oApplicationCacheaddEventListener('updateready', function(e) {
-					if (oApplicationCache.status == oApplicationCache.UPDATEREADY) {
-						// Browser downloaded a new app cache.
-						// Swap it in and reload the page to get the new hotness.
-						oApplicationCache.swapCache();
-						window.location.reload();
-					} else {
-						// Manifest didn't changed. Nothing new to server.
-					}
-				}, false);
-			}
-		},
-
-		_reloadConfig: function() {
-			const sConfig = Storage.get("config");
-			const oConfig = JSON.parse(sConfig);
-
-			const oModel = this.getModel("config");
-			oModel.setData(oConfig);
 		}
+
 	});
 });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,6 +5,12 @@ sap.ui.define([
 ], function(BaseController, SocketHelper, History, Fragment) {
 	"use strict";
 	return BaseController.extend("com.perezjquim.iglivemode.pwa.controller.App", {
+
+		onInit: function(oEvent) {
+			this._validateCache();
+			this._reloadConfig();
+		},
+
 		onHomeButtonPress: function(oEvent) {
 			const oBar = oEvent.getSource();
 			const oToolPage = oBar.getParent();
@@ -19,19 +25,45 @@ sap.ui.define([
 				item: oHomeItem
 			});
 		},
+
 		onMenuButtonPress: function(oEvent) {
 			const oBar = oEvent.getSource();
 			const oToolPage = oBar.getParent();
 			const bIsExpanded = oToolPage.getSideExpanded();
 			oToolPage.setSideExpanded(!bIsExpanded);
 		},
+
 		onNavigationItemSelect: function(oEvent) {
 			const oSelectedItem = oEvent.getParameter("item");
 			const sKey = oSelectedItem.getKey();
 			this.navTo(sKey);
 		},
+
 		onNavButtonPress: function(oEvent) {
 			this.navBack();
+		},
+
+		_validateCache: function() {
+			const oApplicationCache = window.applicationCache;
+			if (oApplicationCache) {
+				oApplicationCacheaddEventListener('updateready', function(e) {
+					if (oApplicationCache.status == oApplicationCache.UPDATEREADY) {
+						const sText = this.getText("updating");
+						this.toast(sText);
+						oApplicationCache.swapCache();
+						window.location.reload();
+					} else {}
+				}, false);
+			}
+		},
+
+		_reloadConfig: function() {
+			const sConfig = Storage.get("config");
+			const oConfig = JSON.parse(sConfig);
+
+			const oModel = this.getModel("config");
+			oModel.setData(oConfig);
 		}
+
 	});
 });
