9c967e3f2585e2b3590f7c2e8d7db35eeb790467
Joaquim Perez <jquim1@hotmail.com>
2021-07-24 14:31:10 +0100

    download process improv

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -15,10 +15,34 @@ sap.ui.define([
 			const oSearchParams = new URLSearchParams(oPromptData);
 			const sSearchParams = decodeURIComponent(oSearchParams.toString());
 
-			const sDownloadUrl = `${this.API_BASE_URL}/crop-video?${sSearchParams}`;
-			window.open(sDownloadUrl);
-
-			this.setBusy(false);
+			const sTimestamp = new Date().getTime();
+			const sDownloadUrl = `${this.API_BASE_URL}/crop-video?${sSearchParams}&=${sTimestamp}`;
+			fetch(sDownloadUrl)
+				.then(oResponse => {
+					if (oResponse.ok) {
+						const sFilename = oResponse.headers.get('content-disposition').split('filename=')[1].split(';')[0];
+						oResponse.blob().then(oData => {
+							const oBlob = new Blob([oData], {
+								type: 'application/octet-stream'
+							});
+							if (window.navigator.msSaveBlob) {
+								window.navigator.msSaveBlob(oBlob, sFilename);
+							} else {
+								const sBlobUrl = window.URL.createObjectURL(oBlob);
+								const oAnchor = document.createElement('a');
+								oAnchor.setAttribute('href', sBlobUrl);
+								oAnchor.setAttribute('download', sFilename);
+								oAnchor.click();
+							}
+						});
+					} else {
+						throw new Error();
+					}
+				})
+				.catch((oError) => {
+					this.setBusy(false);
+					this.toast(`Error! - ${oError}`);
+				});
 		}
 
 	});
